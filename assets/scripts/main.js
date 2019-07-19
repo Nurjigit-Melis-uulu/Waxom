1ba29a513de03522b43a37d9d43f178eb95eb316
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-07-19 14:03:20 +0600

    fix carousel script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 let slides = document.querySelectorAll('.slides div[class^="slide-"]');
 let pointers = document.querySelectorAll(".pointers button");
+let sideButtons = document.querySelectorAll(".side_btn");
 let activeSlide = 0;
 
 for (let i = 0; i < pointers.length; i++) {
@@ -13,6 +14,42 @@ for (let i = 0; i < pointers.length; i++) {
   });
 }
 
+for (let i = 0; i < sideButtons.length; i++) {
+  sideButtons[i].addEventListener("click", function() {
+    if (this.getAttribute("data-direction") === "right" && activeSlide < 4) {
+      activeSlide++;
+    }
+
+    if (this.getAttribute("data-direction") === "left" && activeSlide > 0) {
+      activeSlide--;
+    }
+
+    console.log(activeSlide);
+    activationButtons();
+  });
+}
+
+function activationButtons(params) {
+  for (let i = 0; i < pointers.length; i++) {
+    pointers.forEach(element => {
+      element.className = "";
+    });
+    pointers[activeSlide].className = "active";
+
+    for (let i = 0; i < sideButtons.length; i++) {
+      if (activeSlide === 4) {
+        sideButtons[1].disabled = true;
+      } else if (activeSlide === 0) {
+        sideButtons[0].disabled = true;
+      } else {
+        sideButtons[0].disabled = false;
+        sideButtons[1].disabled = false;
+      }
+    }
+    activationSlide();
+  }
+}
+
 activationSlide();
 
 function activationSlide() {
@@ -21,5 +58,3 @@ function activationSlide() {
     slides[activeSlide].style.display = "flex";
   }
 }
-
-console.log(activeSlide);
