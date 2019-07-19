54a27bc33963e03b21a21e1ed3aaa7dd64ef421d
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-07-19 13:13:49 +0600

    added script for buttons in pointers

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,25 @@
 let slides = document.querySelectorAll('.slides div[class^="slide-"]');
+let pointers = document.querySelectorAll(".pointers button");
 let activeSlide = 0;
 
-for (let i = 0; i < slides.length; i++) {
-  slides[i].style.display = "none";
-  slides[activeSlide].style.display = "flex";
+for (let i = 0; i < pointers.length; i++) {
+  pointers[i].addEventListener("click", function() {
+    activeSlide = i;
+    pointers.forEach(element => {
+      element.className = "";
+    });
+    this.className = "active";
+    activationSlide();
+  });
 }
+
+activationSlide();
+
+function activationSlide() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    slides[activeSlide].style.display = "flex";
+  }
+}
+
+console.log(activeSlide);
